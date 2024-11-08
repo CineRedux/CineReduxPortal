268ab55596342a8e947029d90c2db26e31d5524d
Tshiamo Thekiso ST10132516 <st10132516@vcconnect.edu.za>
2024-11-09 01:15:42 +0200

    disable registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,31 +5,31 @@ import { validationResult } from 'express-validator';
 
 
 // Register user
-export const registerUser = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+// export const registerUser = async (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
 
-    const { email, fullName, idNumber, accountNumber, password } = req.body;
+//     const { email, fullName, idNumber, accountNumber, password } = req.body;
 
-    try {
-        const username = email;
+//     try {
+//         const username = email;
         
-        const existingUser = await User.findOne({ username });
-        if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
-        }
+//         const existingUser = await User.findOne({ username });
+//         if (existingUser) {
+//             return res.status(400).json({ message: 'User already exists' });
+//         }
 
 
-        const newUser = new User({ email, fullName, idNumber, accountNumber, username, password });
-        await newUser.save();
+//         const newUser = new User({ email, fullName, idNumber, accountNumber, username, password });
+//         await newUser.save();
 
-        res.status(201).json({ message: 'User registered successfully!', username });
-    } catch (err) {
-        res.status(500).json({ error: 'Registration failed', err });
-    }
-};
+//         res.status(201).json({ message: 'User registered successfully!', username });
+//     } catch (err) {
+//         res.status(500).json({ error: 'Registration failed', err });
+//     }
+// };
 
 // Login user
 export const loginUser = async (req, res) => {
