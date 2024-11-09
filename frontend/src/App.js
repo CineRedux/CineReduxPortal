610f9e732adb7f414987bae44d14ff579c32cd79
Tshiamo Thekiso ST10132516 <st10132516@vcconnect.edu.za>
2024-11-09 15:29:17 +0200

    disable registration in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,6 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
             <Route 
               path="/dashboard/customer/dashboard" 
